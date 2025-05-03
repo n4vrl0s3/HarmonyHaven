b539edbc343acbaebd0d73a18ae807f4bd2bbae7
n4vrl0s3 <n4vrl0s3@gmail.com>
2025-05-03 15:29:10 +0700

    Splash Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:harmonyhaven/core/configs/theme/app_theme.dart';
+import 'package:harmonyhaven/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme,
-      home: Container()
+      debugShowCheckedModeBanner: false,
+      home: const SplashPage()
     );
   }
 }
